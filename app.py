16e443ee61971c5024afd23a1cd8978402fb6e81
Terrence2333 <TU_CORREO_DE_GITHUB>
2026-02-20 18:38:19 -0500

    Actualización Automática Nivel Dios - Terrence Mayorga

--- app.py
+++ app.py
@@ -4,24 +4,31 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def index(): return render_template('index.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
-def about(): return render_template('about.html')
+def about():
+    return render_template('about.html')
 
 @app.route('/servicios')
-def servicios(): return render_template('servicios.html')
+def servicios():
+    return render_template('servicios.html')
 
 @app.route('/proyectos')
-def proyectos(): return render_template('proyectos.html')
+def proyectos():
+    return render_template('proyectos.html')
 
 @app.route('/pagos')
-def pagos(): return render_template('pagos.html')
+def pagos():
+    return render_template('pagos.html')
 
 @app.route('/exito')
-def exito(): return render_template('exito.html')
+def exito():
+    return render_template('exito.html')
 
 if __name__ == '__main__':
+    # Puerto 10000 obligatorio para Render
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
     
