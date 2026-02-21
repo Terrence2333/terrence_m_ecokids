fccd75423438af7cf35dbb40f04bcd9577084f70
Terrence2333 <TU_CORREO_DE_GITHUB>
2026-02-20 22:32:58 -0500

    Actualización Automática Nivel Dios - Terrence Mayorga

---
+++ templates/actualizar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Kernel Updater para Kali Linux
+echo -e "\e[1;33m[!] SINCRONIZANDO INTERFAZ TERRENCE M...\e[0m"
+git add .
+git commit -m "RELEASE: God Mode Interface v3 - Full Navigation & Biometrics"
+git push origin main --force
+echo -e "\e[1;32m[+] IMPERIO ACTUALIZADO Y EN LÍNEA.\e[0m"
