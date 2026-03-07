1356e9455580bce4664f14a049c7812cb94b8312
Terrence2333 <TU_CORREO_DE_GITHUB>
2026-03-07 08:40:02 -0500

    Tarea 12: Limpieza de archivos temporales y optimización final

--- templates/actualizar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Despliegue de Sistema con Voz - Director Terrence M
-echo -e "\e[1;34m[!] SUBIENDO CÓDIGO NIVEL DIOS CON VOZ INTEGRADA...\e[0m"
-git add .
-git commit -m "FINAL_REBUILD: Chatbot Voice, Audio & Biometrics Operational"
-git push origin main --force
-echo -e "\e[1;32m[+] SISTEMA ACTUALIZADO EN RENDER.\e[0m"
